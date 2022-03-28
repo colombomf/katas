eec87fdd9ab325b9493a3d45fef24ff3263a1fe1
colombomf <mischa@gmail.com>
2022-03-28 20:46:46 -0300

    Added unary negation operator

---
+++ 8 kyu/unary_negation.js
@@ -0,0 +1,8 @@
+
+/* 
+Given an integer or a floating-point number, find its opposite.
+*/
+
+function findTheOpposite(number) {
+    return (-number) //the unary negation operator (-) returns the negative of its operand.
+}
