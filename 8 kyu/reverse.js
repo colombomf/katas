4f443326938e6747930d3307644342d464e76bfe
Michelle <53919326+colombomf@users.noreply.github.com>
2022-04-11 14:05:29 -0300

    Added reverse challenge (arrow function).

---
+++ 8 kyu/reverse.js
@@ -0,0 +1,15 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+//The split() method splits a string into an array of substrings
+//The reverse() method reverses the order of elements in the array of substrings created by split()
+//The join() method combines the elements of the array reversed by reverse() and stores it as a string value
+
+solution = (str) => str.split('').reverse().join('')
+
+
+
+
