7ad8aefdeb07a9bf6571bcc970dca43c4569edb7
Michelle <53919326+colombomf@users.noreply.github.com>
2022-04-28 01:21:07 -0300

    Added the lowest and highest integer in a string of space separated numbers.

---
+++ 7 kyu/returnHighesAndtLowest.js
@@ -0,0 +1,32 @@
+
+/* 
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+Notes
+All numbers are valid Int32, no need to validate them.
+There will always be at least one number in the input string.
+Output string must be two numbers separated by a single space, and highest number is first.
+*/
+
+function highAndLow(numbers) {
+    const arr = numbers.split(' ')
+    let highestNum = arr[0]
+    let lowestNum = arr[0]
+  
+    for(let num of arr) {
+        if(Number(num) > highestNum) {
+          highestNum = num
+        }
+  
+        if(Number(num) < lowestNum) {
+          lowestNum = num
+        }
+    }
+  
+    return highestNum + ' ' + lowestNum
+}
