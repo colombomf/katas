7181270fe4890e6b087a928334699e347a96cab4
Michelle <53919326+colombomf@users.noreply.github.com>
2022-05-14 00:02:08 -0300

    Added sum of two arrays with concat/reduce.

---
+++ 8 kyu/sumTwoArrays.js
@@ -0,0 +1,9 @@
+
+/* 
+Get the sum of two arrays... Actually the sum of all their elements.
+Each array includes only integer numbers. Output is a number too.
+*/
+
+arrayPlusArray = (arr1, arr2) => arr1.concat(arr2).reduce((acc, cur) => acc + cur)
+
+
